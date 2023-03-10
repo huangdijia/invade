9961b2df6ca6ea93fb77adfbe0b391c76931cf4a
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2023-03-10 17:51:45 +0800

    Bump pest to 2.0

--- tests/SubClassTest.php
+++ tests/SubClassTest.php
@@ -7,21 +7,19 @@ declare(strict_types=1);
  * @link     https://github.com/huangdijia/invade
  * @contact  huangdijia@gmail.com
  */
+set_error_handler(fn ($severity, $message, $file, $line) => throw new Exception($message, 1));
+
 beforeEach(function () {
     $this->bar = new Bar();
 });
 
 it('can not read a private property of parent class', function () {
-    $privateValue = invade($this->bar)->privateProperty;
-
-    expect($privateValue)->toBe('privateValue');
-})->expectExceptionMessage('Undefined property: Bar::$privateProperty');
+    invade($this->bar)->privateProperty;
+})->throws('Undefined property: Bar::$privateProperty');
 
 it('can call the private method of parent class', function () {
-    $returnValue = invade($this->bar)->privateMethod();
-
-    expect($returnValue)->toBe('private return value');
-})->expectErrorMessage('Call to private method Foo::privateMethod() from scope Bar');
+    invade($this->bar)->privateMethod();
+})->throws('Call to private method Foo::privateMethod() from scope Bar');
 
 class Foo
 {
