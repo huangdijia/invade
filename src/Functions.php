b90b3afb8a362ad3365739f3e71191dbb1ebf1c6
huangdijia <huangdijia@gmail.com>
2023-06-30 20:22:36 +0800

    Remove Invader

--- src/Functions.php
+++ src/Functions.php
@@ -16,8 +16,42 @@ if (! function_exists('invade')) {
      * @param T $object
      * @return Invader<T>
      */
-    function invade(object $object): Invader
+    function invade(object $object): object
     {
-        return new Invader($object);
+        return new class($object) {
+            private $get;
+
+            private $set;
+
+            private $call;
+
+            public function __construct(object $obj)
+            {
+                $this->get = (
+                    function ($name) { return $this->{$name}; }
+                )->bindTo($obj, $obj);
+                $this->set = (
+                    function ($name, $value) { $this->{$name} = $value; }
+                )->bindTo($obj, $obj);
+                $this->call = (
+                    function ($name, $params) { return $this->{$name}(...$params); }
+                )->bindTo($obj, $obj);
+            }
+
+            public function __get(string $name): mixed
+            {
+                return ($this->get)($name);
+            }
+
+            public function __set(string $name, mixed $value): void
+            {
+                ($this->set)($name, $value);
+            }
+
+            public function __call(string $name, array $params = []): mixed
+            {
+                return ($this->call)($name, $params);
+            }
+        };
     }
 }
